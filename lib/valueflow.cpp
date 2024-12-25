97a857197560af9e837c6560de881e624de8b2d5
firewave <firewave@users.noreply.github.com>
2024-12-25 16:48:57 +0100

    valueflow.cpp: avoid unnecessary copy in `valueFlowAfterAssign()`

--- lib/valueflow.cpp
+++ lib/valueflow.cpp
@@ -4226,7 +4226,7 @@ static void valueFlowAfterAssign(const TokenList &tokenlist,
                         continue;
                     ids.insert(value.tokvalue->exprId());
                 }
-                for (ValueFlow::Value value : values) {
+                for (ValueFlow::Value& value : values) {
                     if (!value.isSymbolicValue())
                         continue;
                     const Token* expr = value.tokvalue;
