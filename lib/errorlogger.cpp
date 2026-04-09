5fb9f6f1b2803f390a18b4861af7770dd019d4b3
William Jakobsson <william.jakobsson@cppcheck.com>
2026-04-09 14:14:13 +0200

    fix-14657

--- lib/errorlogger.cpp
+++ lib/errorlogger.cpp
@@ -1082,13 +1082,12 @@ std::string getGuideline(const std::string &errId, ReportType reportType,
     case ReportType::misraC2025:
         if (errId.rfind("misra-c20", 0) == 0 || errId.rfind("premium-misra-c-20", 0) == 0) {
             auto pos1 = errId.find("20") + 5;
+            if (pos1 >= errId.size())
+                break;
             if (errId.compare(pos1,4,"dir-",0,4) == 0)
                 pos1 += 4;
             const auto endpos = errId.find('-', pos1);
-            if (endpos != std::string::npos)
-                guideline = errId.substr(pos1, endpos-pos1);
-            else
-                guideline = errId.substr(pos1);
+            guideline = errId.substr(pos1, endpos-pos1);
         }
         break;
     case ReportType::misraCpp2008:
