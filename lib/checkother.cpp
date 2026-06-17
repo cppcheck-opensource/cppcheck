0a0dcf2b468116e7f1fbfc54ba36f92125774029
chrchr-github <noreply@github.com>
2026-06-17 20:07:06 +0200

    Simplify

--- lib/checkother.cpp
+++ lib/checkother.cpp
@@ -4052,7 +4052,7 @@ void CheckOtherImpl::checkFuncArgNamesDifferent()
                     break;
                 }
                 // skip over templates and arrays
-                if (decl->link() && (!Token::Match(decl, "[()]") || Token::simpleMatch(decl->tokAt(-1), ") (")))
+                if (decl->link() && precedes(decl, decl->link()) && !Token::Match(decl, "( [*&]"))
                     decl = decl->link();
                 else if (decl->varId())
                     declarations[j] = decl;
