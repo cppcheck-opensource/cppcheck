2bd7e6f7035cf912423aefcccc64f07f04a49d52
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2026-07-28 15:13:27 +0200

    Update testleakautovar.cpp

--- test/testleakautovar.cpp
+++ test/testleakautovar.cpp
@@ -211,6 +211,7 @@ private:
         TEST_CASE(inlineFunction); // #3989
 
         TEST_CASE(smartPtrInContainer); // #8262
+        TEST_CASE(unconditionalScope);
 
         TEST_CASE(functionCallCastConfig); // #9652
         TEST_CASE(functionCallLeakIgnoreConfig); // #7923
@@ -3176,6 +3177,24 @@ private:
               dinit(CheckOptions, $.cpp = true)
               );
         ASSERT_EQUALS("", errout_str());
+    }
+
+    void unconditionalScope() { // #14945
+        check("void f() {\n"
+              "    {\n"
+              "        int* p = new int;\n"
+              "        *p = 1;\n"
+              "    }\n"
+              "    {\n"
+              "        int* q = new int;\n"
+              "        *q = 2;\n"
+              "        delete q;\n"
+              "    }\n"
+              "    int* r = new int;\n"
+              "    *r = 3;\n"
+              "    delete r;\n"
+              "}\n", dinit(CheckOptions, $.cpp = true));
+        ASSERT_EQUALS("[test.cpp:5:5]: (error) Memory leak: p [memleak]\n", errout_str());
     }
 
     void functionCallCastConfig() { // #9652
