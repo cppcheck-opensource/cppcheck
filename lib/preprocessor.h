a7c459781b34e9ef1ed0422acb1d6a99b63a5385
glank <glankk@users.noreply.github.com>
2026-08-19 22:37:31 +0200

    14974

--- lib/preprocessor.h
+++ lib/preprocessor.h
@@ -122,7 +122,9 @@ public:
 
     std::vector<RemarkComment> getRemarkComments(const simplecpp::TokenList &tokens) const;
 
-    bool loadFiles(std::vector<std::string> &files);
+    bool loadAllIncludes(std::vector<std::string> &files);
+
+    simplecpp::FileData *loadFile(std::vector<std::string> &files, const std::string &file);
 
     void removeComments();
 
@@ -163,6 +165,8 @@ public:
         mFileCache.set_load_callback(std::move(cb));
     }
 
+    simplecpp::FileDataCache mFileCache;
+
 private:
 
     /**
@@ -181,8 +185,6 @@ private:
 
     const Settings& mSettings;
     ErrorLogger &mErrorLogger;
-
-    simplecpp::FileDataCache mFileCache;
 
     /** filename for cpp/c file - useful when reporting errors */
     std::string mFile0; // TODO: this is never set
